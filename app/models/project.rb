0c3b586736babe27257a60d6703ebee3d5795708
mdavies <ms.melissadavies@gmail.com>
2021-07-19 14:58:26 +0200

    Add uniform styling for tasks and projects pages

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,4 +1,26 @@
 class Project < ApplicationRecord
   has_many :tasks
   belongs_to :user
+
+  # def badge_color
+  #   case status
+  #   when 'not-started'
+  #     'secondary'  
+  #   when 'in-progress'
+  #     'info'
+  #   when 'complete'
+  #     'success'
+  #   end 
+  # end 
+
+  # def project_status
+  #   if tasks.all? { |task| task.complete? }
+  #     'complete'
+  #   elsif 
+  #     tasks.any? { |task| task.in_progress? }
+  #     'in-progress'
+  #   else 
+  #     'not-started'
+  #   end 
+  # end 
 end
